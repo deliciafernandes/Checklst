43e036118055c202cf090b373a83062eff9ca030
Delicia Fernandes <fernandesdelicia07@gmail.com>
2020-11-11 02:05:38 +0530

    Fonts added

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print('home page');
-
     return Scaffold();
   }
 }
