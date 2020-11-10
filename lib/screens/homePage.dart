9d16c0d9e13ec896b127e4744a98380966260515
Delicia Fernandes <fernandesdelicia07@gmail.com>
2020-11-11 02:16:27 +0530

    Homepage ongoing

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(),
+    );
   }
 }
